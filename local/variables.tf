f912fa95df918c34c1abe1a832d4ae2e078dc85d
Nick Thompson <nickbdt86@gmail.com>
2024-10-07 01:00:42 -0400

    update local

--- local/variables.tf
+++ local/variables.tf
@@ -1,4 +1,34 @@
 variable "minikube_ip" {
-  description = "The IP address of the Minikube cluster"
   type        = string
+  description = "The IP address of the Minikube cluster"
+}
+
+variable "DATABASE_USER" {
+  type        = string
+  description = "Database user for the PostgreSQL instance"
+}
+
+variable "DATABASE_PASSWORD" {
+  type        = string
+  description = "Database password for the PostgreSQL instance"
+}
+
+variable "DATABASE_NAME" {
+  type        = string
+  description = "Database name for the PostgreSQL instance"
+}
+
+variable "NEXT_PUBLIC_API_URL" {
+  type        = string
+  description = "Public API URL"
+}
+
+variable "NEXT_PUBLIC_WEBAPP_URL" {
+  type        = string
+  description = "Public Webapp URL"
+}
+
+variable "tls_secret_name" {
+  type        = string
+  description = "TLS secret for HTTPS"
 }
