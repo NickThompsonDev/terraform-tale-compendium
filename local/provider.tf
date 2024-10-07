6141f771d32b523625a820d179c2c27a2ec4afd5
Nick Thompson <nickbdt86@gmail.com>
2024-10-07 04:16:14 -0400

    update Jenkinsfile for terraform missing variables

--- local/provider.tf
+++ local/provider.tf
@@ -1,9 +1,13 @@
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = "/root/.kube/config"  # Path to the Kubernetes config inside the Jenkins container
+  host                   = "https://${MINIKUBE_IP}:8443"  # Minikube Kubernetes API
+  client_certificate     = file("/var/lib/minikube/certs/client.crt")  # Path to the client certificate
+  client_key             = file("/var/lib/minikube/certs/client.key")  # Path to the client key
+  cluster_ca_certificate = file("/var/lib/minikube/certs/ca.crt")  # Path to the cluster CA certificate
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    config_path = "/root/.kube/config"  # Path to the Kubernetes config for Helm as well
   }
 }
